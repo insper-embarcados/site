90c1b4c514bdcea3ce0327b174747acfbc2cf465
Corsi <corsiferrao@gmail.com>
2026-01-26 15:54:50 -0300

    fix

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,28 +1,17 @@
 <script setup>
-import { useData } from 'vitepress'
-import { computed } from 'vue'
-import SidebarLinks from './SidebarLinks.vue'
-
-// Import the Lando Layout directly
-import VPLLayout from '@lando/vitepress-theme-default-plus/components/VPLLayout.vue'
-
-const { frontmatter } = useData()
-
-// Extract sidebar links from frontmatter
-const sidebarLinks = computed(() => {
-  console.log('[SidebarLinks DEBUG] frontmatter:', frontmatter.value)
-  if (!frontmatter.value || !frontmatter.value.sidebar_links) {
-    return []
-  }
-  console.log('[SidebarLinks DEBUG] Found links:', frontmatter.value.sidebar_links)
-  return frontmatter.value.sidebar_links
-})
+import DefaultTheme from 'vitepress/theme'
+const { Layout, VPSidebar, VPNav, VPContent } = DefaultTheme
 </script>
 
 <template>
-  <VPLLayout>
-    <template #aside-ads-before>
-      <SidebarLinks :links="sidebarLinks" />
+  <Layout>
+    <template #sidebar>
+      <VPSidebar />
     </template>
-  </VPLLayout>
+    <!-- Add hero, features, and actions here to mimic home layout -->
+    <div class="hero-section">
+      <!-- Hero content -->
+    </div>
+    <VPContent />
+  </Layout>
 </template>
